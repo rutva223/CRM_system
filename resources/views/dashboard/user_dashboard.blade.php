925bcabd673a676eef25178f395e0c7737969c37
TheRutica <113928857+TheRutica@users.noreply.github.com>
2024-01-18 21:58:02 +0530

    error solve

--- resources/views/dashboard/user_dashboard.blade.php
+++ resources/views/dashboard/user_dashboard.blade.php
@@ -20,9 +20,9 @@
                     </div>
                     <div class="col-sm-5 text-center text-sm-left">
                         <div class="card-body pb-0 px-0 px-md-4">
-                            <img src="{{ asset('assets/img/illustrations/man-with-laptop-light.png') }}" height="140"
+                            <img  height="140"
                                 alt="View Badge User" data-app-dark-img="illustrations/man-with-laptop-dark.png"
-                                data-app-light-img="illustrations/man-with-laptop-light.png" />
+                                src="{{ asset('assets/img/illustrations/man-with-laptop-light.png') }}" />
                         </div>
                     </div>
                 </div>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -25,8 +25,10 @@
     <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/boxicons.css') }}" />
 
     <!-- Core CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/vendor/css/core.css') }}" class="template-customizer-core-css" />
-    <link rel="stylesheet" href="{{ asset('assets/vendor/css/theme-default.css') }}"
+    <link rel="stylesheet" href="{{ asset('assets/vendor/css/rtl/core.css') }}" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/css/rtl/theme-default.css') }}"
+        class="template-customizer-theme-css" />
+        <link rel="stylesheet" href="{{ asset('assets/vendor/css/rtl/theme-default.css') }}"
         class="template-customizer-theme-css" />
     <link rel="stylesheet" href="{{ asset('assets/css/demo.css') }}" />
 
