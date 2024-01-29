4313ab620d9c4c1459d02cef2f6298453c69072f
rutva223 <rutvapatel203@gmail.com>
2024-01-29 20:59:34 +0530

    permmision & data table

--- resources/views/role/index.blade.php
+++ resources/views/role/index.blade.php
@@ -30,7 +30,7 @@
     <div class="cm-content-body form excerpt">
         <div class="card-body pb-4">
             <div class="table-responsive">
-                <table class="table">
+                <table class="datatable">
                     <thead>
                         <tr>
                             <th>{{__('Role')}} </th>
@@ -50,25 +50,25 @@
                             </td>
                             <td class="Action">
                                 <span>
-                                    <!-- @can('edit role') -->
-                                        <a href="#!" data-size="lg" data-url="{{ route('roles.edit',$role->id) }}" data-ajax-popup="true" class="btn btn-warning btn-sm content-icon" data-title="{{__('Edit User')}}">
-                                            <i class="fa fa-edit"></i>
-                                        </a>
-                                    <!-- @endcan
-                                    @can('delete role') -->
-                                        {!! Form::open(['method' => 'DELETE', 'route' => ['roles.destroy', $role->id]]) !!}
-                                            <a href="javascript:;" class="btn btn-danger btn-sm content-icon js-sweetalert" title="Delete data">
-                                                <i class="fa fa-trash"></i>
-                                            </a>
-                                        {!! Form::close() !!}
-                                    <!-- @endcan -->
+                                    @can('edit roles')
+                                    <a href="#!" data-size="lg" data-url="{{ route('roles.edit',$role->id) }}" data-ajax-popup="true" class="btn btn-warning btn-sm content-icon" data-title="{{__('Edit User')}}">
+                                        <i class="fa fa-edit"></i>
+                                    </a>
+                                    @endcan
+                                    @can('delete roles')
+                                    {!! Form::open(['method' => 'DELETE', 'route' => ['roles.destroy', $role->id]]) !!}
+                                    <a href="javascript:;" class="btn btn-danger btn-sm content-icon js-sweetalert" title="Delete data">
+                                        <i class="fa fa-trash"></i>
+                                    </a>
+                                    {!! Form::close() !!}
+                                    @endcan
                                 </span>
                             </td>
                         </tr>
                         @endforeach
                     </tbody>
                 </table>
-                <div class="d-flex align-items-center justify-content-between flex-wrap">
+                <!-- <div class="d-flex align-items-center justify-content-between flex-wrap">
                     <p class="mb-2 me-3">Page 1 of 5, showing 2 records out of 8 total, starting on record 1, ending on
                         2</p>
                     <nav aria-label="Page navigation example mb-2">
@@ -80,7 +80,7 @@
                             <li class="page-item"><a class="page-link " href="javascript:void(0);"><i class="fa-solid fa-angle-right"></i></a></li>
                         </ul>
                     </nav>
-                </div>
+                </div> -->
             </div>
         </div>
     </div>
